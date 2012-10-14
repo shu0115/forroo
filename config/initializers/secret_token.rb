7477a8439f90d8a160ee8a457ea3cdb59df78c85
shu0115 <s.matsumoto0115@gmail.com>
2012-10-14 18:30:12 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forroo::Application.config.secret_token = '881a5c462c1124f6afdacaf90e4115c5addf3a7e96202b40161f8d86da6ca3cd0eaf350314b7df2838b60b1c27e0dd1782a079372d2cd2e7ce7fd8a286bb74e4'
